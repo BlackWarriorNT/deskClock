75c4a26976e1c0d13c37db62c0aaeef82716a28a
Amarok <Amarok@AMAROK>
2021-08-08 11:14:34 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace deskClock
+{
+    partial class deskClock
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(deskClock));
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "время";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDoubleClick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.progressBar1.Location = new System.Drawing.Point(8, 46);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(0);
+            this.progressBar1.Maximum = 60;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(224, 10);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // deskClock
+            // 
+            this.AccessibleDescription = "Desktop widget clock";
+            this.AccessibleName = "deskClock";
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Clock;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(241, 67);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(10, 5);
+            this.Name = "deskClock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "deskClock";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Media;
+using System.Windows.Forms;
+
+namespace deskClock
+{
+    public partial class deskClock : Form
+    {
+        Timer timer = new Timer();
+        public deskClock()
+        {
+            InitializeComponent();
+            byte fontSize = 14;
+            this.Opacity = 0.75;
+            this.Size = new Size(fontSize * 9, fontSize + 8);
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width - this.Width, System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height - this.Height);
+            //this.Location = new Point(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width / 2, System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height / 2);
+            label1.Font = new Font("Tahoma", fontSize - 2, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(204, 207, 255);
+            label1.Width = this.Width;
+            label1.Height = this.Height - 2;
+            label1.BackColor = Color.FromArgb(36, 48, 54);
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.AutoSize = false;
+            label1.Location = new Point(0, 0);
+            progressBar1.Location = new Point(0, label1.Height);
+            progressBar1.Height = 2;
+            progressBar1.Width = label1.Width;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            curTime();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(updateTime);
+            timer.Start();
+        }
+
+        private void sayTime(byte hour)
+        {
+            switch (hour)
+            {
+                case 0:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._00))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 1:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._01))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 2:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._02))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 3:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._03))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 4:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._04))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 5:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._05))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 6:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._06))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 7:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._07))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 8:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._08))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 9:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._09))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 10:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._10))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 11:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._11))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 12:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._12))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 13:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._13))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 14:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._14))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 15:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._15))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 16:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._16))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 17:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._17))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 18:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._18))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 19:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._19))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 20:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._20))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 21:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._21))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 22:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._22))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                case 23:
+                    using (MemoryStream fileOut = new MemoryStream(Properties.Resources._23))
+                    using (GZipStream gz = new GZipStream(fileOut, CompressionMode.Decompress))
+                        new SoundPlayer(gz).Play();
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void curTime()
+        {
+            progressBar1.Value = DateTime.Now.Second;
+            label1.Text = DateTime.Now.ToString("HH : mm : ss");
+        }
+
+        private void updateTime(object sender, EventArgs e)
+        {
+            curTime();
+            if (DateTime.Now.Minute == 0 && DateTime.Now.Second == 0) sayTime(Convert.ToByte(DateTime.Now.Hour));
+
+        }
+        private void label1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+    }
+}
